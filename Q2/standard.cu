bec3038729ae8fd9efbe190fdbb830610bacb34a
EthanRush <rush.e@husky.neu.edu>
2022-12-07 02:46:04 -0500

    maybe it was the size?

--- Q2/standard.cu
+++ Q2/standard.cu
@@ -79,7 +79,7 @@ dim3 threadsPerBlock(N,N,N);
   
  
 	// Size, in bytes, of each vector
-    size_t bytes = N*sizeof(double);
+    size_t bytes = N*sizeof(float);
  
 	 // Allocate memory for each vector on host
     h_a = (float*)malloc(bytes);
@@ -130,7 +130,7 @@ for (i=1; i<N-1; i++)
    for (j=1; j<N-1; j++)  
      for (k=1; k<N-1; k++) {
 			float op1 = a[I3D(i,j,k)];
-			float op2 = h_a[i][j][k]
+			float op2 = h_a[i][j][k];
 			if( op1 == op2){
 			continue;
 			}
